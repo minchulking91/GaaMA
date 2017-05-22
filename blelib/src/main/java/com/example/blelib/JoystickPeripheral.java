3751a6e70ca7f8bb56db84ec20b97344cdfece0c
minchulKim <minchulking91@gmail.com>
2017-05-22 20:48:03 +0900

    commit

--- blelib/src/main/java/com/example/blelib/JoystickPeripheral.java
+++ blelib/src/main/java/com/example/blelib/JoystickPeripheral.java
@@ -36,56 +36,18 @@ public class JoystickPeripheral extends HidPeripheral {
             END_COLLECTION(0),
     };
 
-    /**
-     * Constructor<br />
-     * Before constructing the instance, check the Bluetooth availability.
-     *
-     * @param context the applicationContext
-     */
+
     public JoystickPeripheral(final Context context) throws UnsupportedOperationException {
         super(context.getApplicationContext(), true, false, false, 10);
+    }
+
+    public void onChangeButtonStatus() {
+        //TODO addReport
     }
 
     @Override
     protected byte[] getReportMap() {
         return REPORT_MAP;
-    }
-
-    /**
-     * Move the joystick pointer
-     *
-     * @param dx delta X (-127 .. +127)
-     * @param dy delta Y (-127 .. +127)
-     * @param dz delta Z (-127 .. +127)
-     * @param leftButton true : button down
-     * @param rightButton true : button down
-     * @param middleButton true : button down
-     */
-    public void movePointer(int dx, int dy, int dz, final boolean leftButton, final boolean rightButton, final boolean middleButton) {
-        if (dx > 127) dx = 127;
-        if (dx < -127) dx = -127;
-        if (dy > 127) dy = 127;
-        if (dy < -127) dy = -127;
-        if (dz > 127) dz = 127;
-        if (dz < -127) dz = -127;
-        byte button = 0;
-        if (leftButton) {
-            button |= 1;
-        }
-        if (rightButton) {
-            button |= 2;
-        }
-        if (middleButton) {
-            button |= 4;
-        }
-
-        final byte[] report = new byte[5];
-        report[0] = (byte) (button & 7);
-        report[1] = (byte) dx;
-        report[2] = (byte) dy;
-        report[3] = (byte) dz;
-
-        addInputReport(report);
     }
 
     @Override
